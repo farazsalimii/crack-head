340059094cad0c79f47c9f3b9af489a25cd83bb6
farazsalimii <98199631+farazsalimii@users.noreply.github.com>
2023-12-10 11:05:53 -0600

    crack head.py

--- cracker.py
+++ cracker.py
@@ -2,51 +2,145 @@ import sys
 import hashlib
 import bcrypt
 
-def main(inMode=-1, inPwd=""):
-    hashMode = 0
+def print_header():
+    print("-------------------------------------\n"\
+          "--- crackhead cracker crack crack ---\n"\
+          "---------------------------------------")
 
-    print("~~~~~~~~~~~~~~~~~~~")
-    print("~~~~~ CRACKER ~~~~~")
-    print("~~~~~~~~~~~~~~~~~~~")
+def get_password():
+    return input("Enter a password: ")
 
-    if inMode != -1:
-        print("Using mode", inMode, "...")
+def get_mode_choice():
+    try:
+        return int(input("\nEnter 0 to change password\n"\
+                          "Enter 1 to use dictionary\n"\
+                          "Enter 2 to brute force\n"\
+                          "Enter 3 to convert to MD5\n"\
+                          "Enter 4 to convert to SHA256\n"\
+                          "Enter 5 to convert to BCrypt\n"\
+                          "Enter 6 to quit\n"))
+    except ValueError:
+        print("Please enter a valid mode!")
+
+def dictionary_crack(pwd, h_mode):
+    pass_list = open("passList.txt", "r").read()
+    word_list = pass_list.splitlines()
     
-    if inPwd == "":
-        pwd = input("Enter a password: ")
-    else:
-        pwd = inPwd
-    
-    while True:
-        count = 0
+    for guess in word_list:
+        if h_mode == 5 and bcrypt.checkpw(guess.encode(), pwd):
+            print("\nCracked password:", guess)
+            return
         
-        if int(inMode) > 0:
-            checkMode(inMode)
-            inMode = -1
-        else:
-            try:
-                mode = int(input("Enter 0 to change password\nEnter 1 to use dictionary\nEnter 2 to ..."))
-                
-                if mode == 0:
-                    pwd = input("Enter a new password: ")
-                elif mode == 1:
-                    # Add code for dictionary mode
-                    pass
-                elif mode == 2:
-                    # Add code for mode 2
-                    pass
-                else:
-                    print("Invalid mode. Please try again.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-            except KeyboardInterrupt:
-                print("\nProgram terminated.")
-                break
-
-def checkMode(mode):
-    # Add code to handle different modes
-    pass
-
-if __name__ == "__main__":
-    main()
+        hashed_guess = to_hash(guess, h_mode) if h_mode > 2 else guess
+        
+        if h_mode != 5 and hashed_guess == pwd:
+            print("\nCracked password:", guess)
+            return
     
+    print("\nDictionary failed :( \nMaybe try brute forcing?")
+
+def brute_crack(pwd, size, h_mode, guess=""):
+    global count
+    global found
+    
+    if size == 0:
+        count += 1
+        if h_mode == 5 and bcrypt.checkpw(guess.encode(), pwd):
+            found = True
+            print("\nCracked password:", guess)
+            return
+        
+        hashed_guess = to_hash(guess, h_mode) if h_mode > 2 else guess
+        
+        if h_mode != 5 and hashed_guess == pwd:
+            found = True
+            print("\nCracked password:", guess)
+            return
+        
+        if h_mode > 2 and count % 3000000 == 0 or count % 7500000 == 0:
+            print("Cracking...")
+    else:
+        for char in range(32, 127):
+            if found:
+                return
+            new_guess = guess + chr(char)
+            brute_crack(pwd, size - 1, h_mode, new_guess)
+
+def to_hash(pwd, mode):
+    if mode == 3:
+        return hashlib.md5(pwd.encode()).hexdigest()
+    elif mode == 4:
+        return hashlib.sha256(pwd.encode()).hexdigest()
+    elif mode == 5:
+        return bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())
+
+def check_mode(mode, h_mode=0):
+    global pwd
+    global hash_mode
+    if mode == 0:
+        hash_mode = 0
+        pwd = get_password()
+    elif mode == 1:
+        dictionary_crack(pwd, h_mode)
+    elif mode == 2:
+        global found
+        found = False
+        for x in range(1, 21):
+            brute_crack(pwd, x, h_mode)
+    elif mode == 6:
+        global run
+        run = False
+    elif 2 < mode < 6:
+        hash_mode = mode
+        pwd = to_hash(pwd, mode)
+        print("\nHashed password:", pwd)
+        if mode == 5:
+            print("Warning: BCrypt cracking is VERY slow.")
+    else:
+        print("Please enter a valid mode!")
+
+def main(in_mode=-1, in_pwd=""):
+    global count
+    global pwd
+    global hash_mode
+    hash_mode = 0
+
+    print_header()
+    
+    if in_mode != -1:
+        print("Using mode " + str(in_mode) + "...")
+    if in_pwd == "":
+        pwd = get_password()
+    else:
+        pwd = in_pwd
+
+    while run:
+        count = 0
+        if int(in_mode) > 0:
+            check_mode(in_mode)
+            in_mode = -1
+        else:
+            mode = get_mode_choice()
+            check_mode(mode, hash_mode)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 2:
+        try:
+            if 0 < int(sys.argv[1]) < 6:
+                main(int(sys.argv[1]))
+            else:
+                print("Please enter a valid mode!")
+        except ValueError:
+            print("Please enter a valid mode!")
+    elif len(sys.argv) == 3:
+        try:
+            if 0 < int(sys.argv[1]) < 6:
+                main(int(sys.argv[1]), sys.argv[2])
+            else:
+                print("Please enter a valid mode!")
+        except ValueError:
+            print("Please enter a valid mode!")
+    elif len(sys.argv) > 2:
+        print("Please enter only one mode and a password!")
